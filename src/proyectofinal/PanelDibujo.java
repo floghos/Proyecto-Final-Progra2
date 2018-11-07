e319bf582aa3fcd153250d5dfabb3689b0f40267
floghos <caleuche94@gmail.com>
2018-11-07 10:17:04 -0300

    remuevo un println() que usabamos para probar, ademas de realinear el codigo

--- src/proyectofinal/PanelDibujo.java
+++ src/proyectofinal/PanelDibujo.java
@@ -37,7 +37,7 @@ public class PanelDibujo extends JPanel implements MouseListener,ActionListener 
         this.am = am;
         this.v=v;
         this.addMouseListener(this);
-	this.caja = new Caja(v.ancho, v.alto);
+		this.caja = new Caja(v.ancho, v.alto);
         this.fps = 30;
         tiempo = new Timer(1000/fps,null);
         tiempo.addActionListener(this);
@@ -53,109 +53,107 @@ public class PanelDibujo extends JPanel implements MouseListener,ActionListener 
         }
 
         dir = new Vector2d(1, 0);
-	gravedad = new Vector2d(0, 1);
-	dir = Vector2d.rotateVector(dir, Math.random()*2*Math.PI);
-	float factor = (float)Math.random()*20 + 5;
-	dir.x *= factor;
-	dir.y *= factor;		
-	this.cir.setAccel(gravedad);
-	this.cir.setVelocity(dir);
+		gravedad = new Vector2d(0, 1);
+		dir = Vector2d.rotateVector(dir, Math.random()*2*Math.PI);
+		float factor = (float)Math.random()*20 + 5;
+		dir.x *= factor;
+		dir.y *= factor;		
+		this.cir.setAccel(gravedad);
+		this.cir.setVelocity(dir);
     }
 	public void accion(String accion) {
-            float factor;
-            switch(accion) {                
-		case "Start":
-                    tiempo.start();
-                    comenzo=true;
-                    break;
-		case "Stop":
-                    tiempo.stop();
-                    break;
-                case "Reset":
-                    comenzo=false;                    
-                    tiempo.stop();
-                    obstaculos.removeAll(obstaculos);
-                    
-                    for(int i=0; i<12;i++){
-                        float x= (float)Math.random()*(v.ancho-215);
-                        float y= (float)Math.random()*(v.alto-25);
-                        float rad=(float)Math.random()*80+5;
-                        Circle aux= new Circle(new Vector2d(x,y),rad,v);
-                        obstaculos.add(aux);
-                    }
-                    dir = new Vector2d(1, 0);
-                    dir = Vector2d.rotateVector(dir, Math.random()*2*Math.PI);
-                    factor = (float)Math.random()*20 + 5;
-                    dir.x *= factor;
-                    dir.y *= factor;
-                    cir.setPos(new Vector2d(200,100));
-                    cir.setAccel(gravedad);
-                    cir.setVelocity(dir);
-                    this.repaint();
-                    break;
-                case "Restart":
-                    comenzo=false;
-                    tiempo.stop();
-                    dir = new Vector2d(1, 0);
-                    dir = Vector2d.rotateVector(dir, Math.random()*2*Math.PI);
-                    factor = (float)Math.random()*20 + 5;
-                    dir.x *= factor;
-                    dir.y *= factor;
-                    cir.setPos(new Vector2d(200,100));
-                    cir.setAccel(gravedad);
-                    cir.setVelocity(dir);
-                    this.repaint();
-                    break;
-            }
-	}
-        public void direccionInicial(String accion){            
-            if(!comenzo){
-                switch(accion){
-                case "<--":
-                    dir=rotateVector(dir,-Math.PI/36);                           
-                    break;                    
-                case "-->":                    
-                    dir=rotateVector(dir,Math.PI/36);                                
-                    break;
+        float factor;
+        switch(accion) {                
+			case "Start":
+                tiempo.start();
+                comenzo=true;
+                break;
+			case "Stop":
+				tiempo.stop();
+                break;
+			case "Reset":
+				comenzo=false;                    
+				tiempo.stop();
+                obstaculos.removeAll(obstaculos);    
+                for(int i=0; i<12;i++){
+					float x= (float)Math.random()*(v.ancho-215);
+                    float y= (float)Math.random()*(v.alto-25);
+                    float rad=(float)Math.random()*80+5;
+                    Circle aux= new Circle(new Vector2d(x,y),rad,v);
+                    obstaculos.add(aux);
                 }
+                dir = new Vector2d(1, 0);
+                dir = Vector2d.rotateVector(dir, Math.random()*2*Math.PI);
+                factor = (float)Math.random()*20 + 5;
+                dir.x *= factor;
+                dir.y *= factor;
+                cir.setPos(new Vector2d(200,100));
+                cir.setAccel(gravedad);
                 cir.setVelocity(dir);
-                repaint();
-            }
+                this.repaint();
+                break;
+            case "Restart":
+				comenzo=false;
+                tiempo.stop();
+                dir = new Vector2d(1, 0);
+                dir = Vector2d.rotateVector(dir, Math.random()*2*Math.PI);
+                factor = (float)Math.random()*20 + 5;
+                dir.x *= factor;
+                dir.y *= factor;
+                cir.setPos(new Vector2d(200,100));
+                cir.setAccel(gravedad);
+                cir.setVelocity(dir);
+                this.repaint();
+                break;
         }
+	}
+	
+    public void direccionInicial(String accion){            
+        if(!comenzo){
+            switch(accion){
+            case "<--":
+                dir=rotateVector(dir,-Math.PI/36);                           
+                break;                    
+            case "-->":                    
+                dir=rotateVector(dir,Math.PI/36);                                
+                break;
+            }
+            cir.setVelocity(dir);
+            repaint();
+		}
+    }
         
     public void paint (Graphics g){
         super.paint(g);
         caja.paint(g);
         g.setColor(Color.red);
-	cir.paint(g);
+		cir.paint(g);
         g.setColor(Color.red);
         g.setColor(Color.black);
         for(int i=0; i<obstaculos.size();++i){
             obstaculos.get(i).paint(g);
         }
         g.setColor(Color.white);
-        if(!comenzo)g.drawLine((int)cir.pos.x, (int)cir.pos.y, (int)(cir.pos.x+dir.x*3), (int)(cir.pos.y+dir.y*3));
-        
+        if(!comenzo)g.drawLine((int)cir.pos.x, (int)cir.pos.y, (int)(cir.pos.x+dir.x*3), (int)(cir.pos.y+dir.y*3));       
     }
-    public void mousePressed(MouseEvent e) {
-		
-        if(am.getModo()==1){//crear
-            System.out.println("crear");
-            if(af.getForma()==1){
+	
+    public void mousePressed(MouseEvent e) {	
+        if(am.getModo()==1){//crear obstaculo
+            if(af.getForma()==1){ //circulo
                 Circle aux= new Circle(new Vector2d(e.getX(),e.getY()),20f,v);
                 obstaculos.add(aux);
                 repaint();
             }
         }
-        if(am.getModo()==2){//borrar
+        if(am.getModo()==2){//borrar obstaculo
             for(int i=obstaculos.size()-1;i>=0;i--){
-                    if(((e.getX()-obstaculos.get(i).pos.x)*(e.getX()-obstaculos.get(i).pos.x)+(e.getY()-obstaculos.get(i).pos.y)*(e.getY()-obstaculos.get(i).pos.y))<=(obstaculos.get(i).getRadius()*obstaculos.get(i).getRadius())){
-                        obstaculos.remove(i);
-                        repaint();
-                        break;
-                    }
+                if(((e.getX()-obstaculos.get(i).pos.x)*(e.getX()-obstaculos.get(i).pos.x)+(e.getY()-obstaculos.get(i).pos.y)*(e.getY()-obstaculos.get(i).pos.y))<=(obstaculos.get(i).getRadius()*obstaculos.get(i).getRadius())){
+                    obstaculos.remove(i);
+                    repaint();
+                    break;
                 }
-            } 
+            }
+        } 
     }
     
     
@@ -176,8 +174,7 @@ public class PanelDibujo extends JPanel implements MouseListener,ActionListener 
         for(int i=0; i<obstaculos.size();i++){
             if(circleVcircle(cir,obstaculos.get(i))&& escalarProyeccion(cir,obstaculos.get(i))>0){
 				cir.translate(Collision.pushOut(cir, obstaculos.get(i)));
-                cir.setVelocity(Vector2d.vecPorEscalar(resColCircle(cir,obstaculos.get(i)), cir.getRestitucion()));
-                
+                cir.setVelocity(Vector2d.vecPorEscalar(resColCircle(cir,obstaculos.get(i)), cir.getRestitucion()));     
             }
         }
     }
